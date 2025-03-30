c598d90ef325230f3dff111a3072fd64efe7abd6
DomainDude <vgxlmangrego@gmail.com>
2025-03-30 19:12:03 +0300

    Create Codes.lua

---
+++ Codes.lua
@@ -0,0 +1,13 @@
+return {
+ KillBricks = 903319803,
+ LPIWrench = 1547909595,
+ IronSevenlessScroll = 964429275,
+ EndlessIceSlasher = 962575893,
+ PieceMaker = 964370969,
+ StaffOfNoob = 965028875,
+ UlitmateRocketLauncher = 975022539,
+ RailRunner = 966706881,
+ MetalWitchVacum = 966957087,
+ MechCarpet = 964648661,
+ F3X = 4901337104,
+}
