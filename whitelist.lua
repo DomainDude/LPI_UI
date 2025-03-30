c843010eb918e16b22cdeca853e4d12304545788
DomainDude <vgxlmangrego@gmail.com>
2025-03-30 19:13:56 +0300

    Create whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,11 @@
+return {
+BullsOfStell = "",
+BullsOfStell1 = "",
+JunkDunkIn = "",
+--BANANA_BANANAooaa = "",
+Manulexy_3010 = "",
+XoxTimoxaxoX = "",
+nothin_057 = "",
+Noob_CheapingBlock = "",
+Nolar_Asker = "",
+}
